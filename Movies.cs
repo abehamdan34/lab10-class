df48a63cbb5d61162492beb93d168d033d5362c8
abraham <abehamdan34@gmail.com>
2020-01-31 08:51:45 -0500

    first turn in

---
+++ Movies.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10MovieClass
+{
+    class Movies
+    {
+        private string title;
+        private string category;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        public Movies()
+        {
+        }
+        public Movies(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+        private static List<string> GetListofMovieCategory(List<Movies> movieList)
+        {
+            List<string> movieCategory = new List<string>();
+            foreach (Movies movie in movieList)
+            {
+                if (!movieCategory.Contains(movie.Category))
+                {
+                    movieCategory.Add(movie.Category);
+                }
+            }
+            return movieCategory;
+        }
+        public static void PrintMovieCategory(List<Movies> movieList)
+        {
+            List<string> movieCategory = GetListofMovieCategory(movieList);
+            foreach (string category in movieCategory)
+            {
+                Console.WriteLine($"{category}, ");
+            }
+        }
+        public static void PrintMoviesByCategory(List<Movies> movieList, string categorySelected)
+        {
+            foreach (Movies movie in movieList)
+            {
+                if (categorySelected == movie.Category)
+                {
+                    Console.WriteLine($"{movie.title} \n");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10MovieClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            List<Movies> bestMoviesEver = new List<Movies>
+            {
+                new Movies("The Matrix" , "scifi"),
+                new Movies("John Wick" , "action"),
+                new Movies("Pulp Fiction" , "action"),
+                new Movies("Step Brothers" , "comedy"),
+                new Movies("Insidious" , "horror"),
+                new Movies("Yes Man" , "comedy"),
+                new Movies("The Conjuring" , "horror"),
+                new Movies("Interstellar" , "scifi"),
+                new Movies("Scream" , "horror"),
+                new Movies("Bad Boys" , "action"),
+            };
+            while (repeat)
+            {
+                Movies.PrintMovieCategory(bestMoviesEver);
+                string userInput = GetUserInput("Please choose a category! or e to exit");
+                if (userInput == "e")
+                {
+                    break;
+                }
+                else if (userInput == "scifi")
+                {
+                    Console.WriteLine("");
+                    Movies.PrintMoviesByCategory(bestMoviesEver, userInput);
+                    repeat = RepeatOptions(repeat);
+                }
+                else if (userInput == "horror")
+                {
+                    Console.WriteLine("");
+                    Movies.PrintMoviesByCategory(bestMoviesEver, userInput);
+                    repeat = RepeatOptions(repeat);
+                }
+                else if (userInput == "action")
+                {
+                    Console.WriteLine("");
+                    Movies.PrintMoviesByCategory(bestMoviesEver, userInput);
+                    repeat = RepeatOptions(repeat);
+                }
+                else if (userInput == "comedy")
+                {
+                    Console.WriteLine("");
+                    Movies.PrintMoviesByCategory(bestMoviesEver, userInput);
+                    repeat = RepeatOptions(repeat);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input please try again!");
+                }
+            }
+        }
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+        public static bool RepeatOptions(bool validate)
+        {
+            string userInput2 = GetUserInput("Would you like to repeat the options? y/n");
+            if (userInput2 == "y")
+            {
+                validate = true;
+            }
+            else if (userInput2 == "n")
+            {
+                validate = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input please try again!");
+                validate = true;
+            }
+            return validate;
+
+        }
+    }
+}
